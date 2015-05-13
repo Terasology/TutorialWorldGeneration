f52a22a98641916789828a8ea7d79a0f948b69be
Martin Steiger <git@martin-steiger.de>
2015-05-13 18:49:51 +0200

    Add facet layer for HouseFacet

---
+++ src/main/java/org/terasology/tutorialWorldGeneration/HouseFacetLayer.java
@@ -0,0 +1,66 @@
+package org.terasology.tutorialWorldGeneration;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.Map.Entry;
+
+import org.terasology.math.geom.Rect2i;
+import org.terasology.math.geom.Vector2i;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.world.generation.Region;
+import org.terasology.world.viewer.layers.AbstractFacetLayer;
+import org.terasology.world.viewer.layers.Renders;
+
+@Renders(value = HouseFacet.class, order = 5000)
+public class HouseFacetLayer extends AbstractFacetLayer {
+
+    private Color fillColor = new Color(224, 128, 128, 128);
+    private Color frameColor = new Color(224, 128, 128, 224);
+
+    @Override
+    public void render(BufferedImage img, Region region) {
+        HouseFacet houseFacet = region.getFacet(HouseFacet.class);
+
+        Graphics2D g = img.createGraphics();
+
+        int dx = region.getRegion().minX();
+        int dy = region.getRegion().minZ();
+        g.translate(-dx, -dy);
+
+        for (Entry<Vector3i, Boolean> entry : houseFacet.getWorldEntries().entrySet()) {
+            if (entry.getValue()) {
+                int extent = 4;
+
+                Vector3i center = entry.getKey();
+                g.setColor(fillColor);
+                g.fillRect(center.x - extent, center.z - extent, 2 * extent, 2 * extent);
+                g.setColor(frameColor);
+                g.drawRect(center.x - extent, center.z - extent, 2 * extent, 2 * extent);
+            }
+        }
+
+        g.dispose();
+    }
+
+    @Override
+    public String getWorldText(Region region, int wx, int wy) {
+        HouseFacet houseFacet = region.getFacet(HouseFacet.class);
+
+        for (Entry<Vector3i, Boolean> entry : houseFacet.getWorldEntries().entrySet()) {
+            if (entry.getValue()) {
+                int extent = 4;
+
+                Vector3i center = entry.getKey();
+                Vector2i min = new Vector2i(center.x - extent, center.z - extent);
+                Vector2i max = new Vector2i(center.x + extent, center.z + extent);
+                if (Rect2i.createFromMinAndMax(min, max).contains(wx, wy)) {
+                    return "House";
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
