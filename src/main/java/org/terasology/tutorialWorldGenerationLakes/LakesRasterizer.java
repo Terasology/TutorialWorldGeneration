13d62a49b8916364488145451c085bee6a38e5a2
Qwertygiy <qwertygiy@gmail.com>
2019-10-04 13:08:45 -0400

    Update to use CoreBlocks

--- src/main/java/org/terasology/tutorialWorldGenerationLakes/LakesRasterizer.java
+++ src/main/java/org/terasology/tutorialWorldGenerationLakes/LakesRasterizer.java
@@ -36,7 +36,7 @@ public class LakesRasterizer implements WorldRasterizerPlugin {
 
     @Override
     public void initialize() {
-        water = CoreRegistry.get(BlockManager.class).getBlock("Core:Water");
+        water = CoreRegistry.get(BlockManager.class).getBlock("CoreBlocks:Water");
     }
 
     @Override
