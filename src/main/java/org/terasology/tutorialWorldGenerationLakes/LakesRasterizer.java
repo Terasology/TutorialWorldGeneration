f54a8e64cad9c226f40aefc793fc7752577d8342
jdrueckert <jd.rueckert@googlemail.com>
2020-06-06 17:52:15 +0200

    chore: update core (spring cleanup) (#15)
    * chore: remove `Core` dependency, increase version
    * feat: add `tutorial` category
    * fix: add `BiomesAPI` dependency
    * fix: add `CoreWorlds` dependency
    * chore: add .gitattributes

--- src/main/java/org/terasology/tutorialWorldGenerationLakes/LakesRasterizer.java
+++ src/main/java/org/terasology/tutorialWorldGenerationLakes/LakesRasterizer.java
@@ -36,7 +36,7 @@ public class LakesRasterizer implements WorldRasterizerPlugin {
 
     @Override
     public void initialize() {
-        water = CoreRegistry.get(BlockManager.class).getBlock("CoreBlocks:Water");
+        water = CoreRegistry.get(BlockManager.class).getBlock("CoreAssets:Water");
     }
 
     @Override
